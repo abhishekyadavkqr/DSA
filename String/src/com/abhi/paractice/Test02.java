551f01c4e7ba08fb52b255d37702d1e241475034
Dark Energy <Dark Energy@DarkEnergy>
2024-06-13 12:15:19 +0530

    String is Palindrome or not?

---
+++ String/src/com/abhi/paractice/Test02.java
@@ -0,0 +1,16 @@
+package com.abhi.paractice;
+
+public class Test02 {
+	
+	public static boolean isPalindrome(String s) {
+        String res= s.replace(" ", "");
+        String re = "";
+        
+        
+        
+        System.out.println(re);
+        boolean r=re.equalsIgnoreCase(s) ? true: false;
+        return r;
+    }
+
+}
