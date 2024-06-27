ee7777bbcffa6ed048a8c18a582e70acc15f0b82
Dark Energy <Dark Energy@DarkEnergy>
2024-06-27 11:36:37 +0530

    Analyzing HashSet

---
+++ AbhiCollection/src/com/abhi/collection/ParacticeHashSet.java
@@ -0,0 +1,13 @@
+package com.abhi.collection;
+
+import java.util.HashSet;
+
+public class ParacticeHashSet {
+
+public static void main(String[] args) {
+	
+	HashSet<Object> hs = new HashSet<Object>();
+	System.out.println(11%16);
+ }
+}
+
